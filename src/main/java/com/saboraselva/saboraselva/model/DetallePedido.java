7d3f00be7e12acf20028bf36d076c17ad7fce5fd
Carlos <CDaniel9806>
2025-06-18 15:46:27 -0500

    a ver nuevo cambio

--- src/main/java/com/saboraselva/saboraselva/model/DetallePedido.java
+++ src/main/java/com/saboraselva/saboraselva/model/DetallePedido.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -29,4 +31,15 @@ public class DetallePedido {
 
     private long cantidad; // Cantidad de productos en el detalle
     private String subtotal;  // Subtotal del detalle de pedido
+
+    @ManyToOne
+    @JoinColumn(name = "id_pedido", nullable = false)/* Un pedido puede tener muchos productos (a través de los detalles): */
+    private Pedido pedido; // Relación con Pedido
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto", nullable = false) /* Un producto puede estar en muchos detalles de pedido: */
+    private Producto producto; // Relación con Producto
+
+    /* pruebaa */
+    
 }   
--- src/main/java/com/saboraselva/saboraselva/model/Pedido.java
+++ src/main/java/com/saboraselva/saboraselva/model/Pedido.java
@@ -1,10 +1,13 @@
 package com.saboraselva.saboraselva.model;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -31,4 +34,7 @@ public class Pedido {
     
     private String estado; // Ejemplo: "Pendiente", "Enviado", "Entregado"
     private Double total; // Total del pedido
+
+    @OneToMany(mappedBy = "pedido")
+    private List<Producto> productos; // Relación con Producto, asumiendo que un pedido puede tener varios productos
 }   
