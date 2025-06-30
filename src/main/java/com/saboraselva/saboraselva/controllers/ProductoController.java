f87e25ccaf35760ef62e5b6a109f94d7f6cd6df9
Carlos <CDaniel9806>
2025-06-30 15:43:43 -0500

    implementando el login

--- src/main/java/com/saboraselva/saboraselva/controllers/ProductoController.java
+++ src/main/java/com/saboraselva/saboraselva/controllers/ProductoController.java
@@ -2,31 +2,18 @@ package com.saboraselva.saboraselva.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import jakarta.validation.Valid;
-
-import java.util.List;
-
-import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.saboraselva.saboraselva.Dto.ProductoDto;
 import com.saboraselva.saboraselva.Service.IProductoService;
-import com.saboraselva.saboraselva.exception.BadRequestException;
 import com.saboraselva.saboraselva.mapper.IProductoMapper;
-import com.saboraselva.saboraselva.model.Producto;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Slf4j
