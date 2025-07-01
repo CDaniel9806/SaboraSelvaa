886a6e95a782fbeee7e5d6a23bbfec366779d629
Carlos <CDaniel9806>
2025-07-01 16:22:10 -0500

    impl login service

--- src/main/java/com/saboraselva/saboraselva/Service/impl/UserService.java
+++ src/main/java/com/saboraselva/saboraselva/Service/impl/UserService.java
@@ -1,12 +1,17 @@
 package com.saboraselva.saboraselva.Service.impl;
 
+import java.util.Collections;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.saboraselva.saboraselva.Repo.IUserRepository;
 import com.saboraselva.saboraselva.model.Usuario;
 
+@Service
 public class UserService implements UserDetailsService {
     
     private IUserRepository userRepository;
@@ -15,5 +20,16 @@ public class UserService implements UserDetailsService {
         this.userRepository = userRepository;
     }
 
-    
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario user = userRepository.findByNombre(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().getName().toString());
+
+        return new org.springframework.security.core.userdetails.User(
+            user.getNombre(),
+            user.getPassword(),
+            Collections.singletonList(authority)
+        );
+    }
 }
